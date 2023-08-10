ef67d38793c813c9a77c6f8d7e2153cf6707182a
qpjiq123@gmail.com <qpjiq123@gmail.com>
2023-08-10 22:35:59 +0900

    JSX 문법 정리

--- ReactApp1/src/App.js
+++ ReactApp1/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
-
+import MyHeader from './MyHeader';
+import MyFooter from './Myfooter';
+import React from 'react';
 function App() {
 
+  // style을 변수처럼 생성해서 사용할 수 있음
+  const style = {
+    bold_text : {
+      color : "green",
+    }
+  }
+
   let name = "권혁찬";
+  let number = 5;
 
   return (
+    // 최상위 태그가 반드시 있어야 한다.
     <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트 {name}</h2>
-      </header>
+      <MyHeader></MyHeader>
+      <h2>안녕 리액트 {name}</h2>
+      <b id="bold_text" style={style.bold_text}>
+        {number}는 : {number%2===0 ? "짝수" : "홀수"}
+      </b>
+      <MyFooter></MyFooter>
     </div>
   );
 }
