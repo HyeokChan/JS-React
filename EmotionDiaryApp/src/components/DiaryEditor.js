81ea389ab50a32d6403a4a8d05a1f24c2b81bb7f
qpjiq123@gmail.com <qpjiq123@gmail.com>
2023-10-12 00:30:14 +0900

    실습프로젝트 일기작성2

--- EmotionDiaryApp/src/components/DiaryEditor.js
+++ EmotionDiaryApp/src/components/DiaryEditor.js
@@ -1,32 +1,34 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef, useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
+import EmotionItem from "./EmotionItem";
 
 const emotionList = [
     {
         emotion_id : 1,
-        emotion_img : process.env.PUBLIC_URL + `/asset/emotion1.png`,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion1.png`,
         emotion_descript : "완전 좋음"
     },
     {
         emotion_id : 2,
-        emotion_img : process.env.PUBLIC_URL + `/asset/emotion2.png`,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion2.png`,
         emotion_descript : "좋음"
     },
     {
         emotion_id : 3,
-        emotion_img : process.env.PUBLIC_URL + `/asset/emotion3.png`,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion3.png`,
         emotion_descript : "그럭저럭"
     },
     {
         emotion_id : 4,
-        emotion_img : process.env.PUBLIC_URL + `/asset/emotion4.png`,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion4.png`,
         emotion_descript : "나쁨"
     },
     {
         emotion_id : 5,
-        emotion_img : process.env.PUBLIC_URL + `/asset/emotion5.png`,
+        emotion_img : process.env.PUBLIC_URL + `/assets/emotion5.png`,
         emotion_descript : "끔찍함"
     },
 ];
@@ -38,6 +40,23 @@ const getStringDate = (date) => {
 const DiaryEditor = () => {
     const navigate = useNavigate();
     const [date, setDate] = useState(getStringDate(new Date()));
+    const [emotion, setEmotion] = useState(3);
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
+    const {onCreate} = useContext(DiaryDispatchContext);
+
+    const handleClickEmote = (emotion) => {
+        setEmotion(emotion);
+    }
+    const handleSubmit = () => {
+        if(content.length < 1){
+            contentRef.current.focue();
+            return;
+        }
+        onCreate(date, content, emotion);
+        navigate("/", {replace:true});
+    }
+
     return (
         <div className="DiaryEditor">
             <MyHeader
@@ -57,8 +76,22 @@ const DiaryEditor = () => {
                     <h4>오늘의 감정</h4>
                     <div className="input_box emotion_list_wrapper">
                         {emotionList.map((it) => (
-                            <div key={it.emotion_id}>{it.emotion_descript}</div>
+                            <EmotionItem key={it.emotion_id} {...it}
+                                onClick={handleClickEmote}
+                                isSelected={it.emotion_id === emotion}/>
                         ))}
+                    </div>
+                </section>
+                <section>
+                    <h4>오늘의 일기</h4>
+                    <div className="input_box text_wrapper">
+                        <textarea ref={contentRef} value={content} onChange={(e)=>setContent(e.target.value)} placeholder="오늘은 어땠나요"/>
+                    </div>
+                </section>
+                <section>
+                    <div className="control_box">
+                        <MyButton text={"취소하기"} onClick={()=>navigate(-1)}/>
+                        <MyButton text={"작성완료"} type={"positive"} onClick={handleSubmit}/>
                     </div>
                 </section>
             </div>
