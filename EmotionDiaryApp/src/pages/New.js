ff03df12efa96a1f0250ef90228f1410a8672e03
qpjiq123@gmail.com <qpjiq123@gmail.com>
2023-10-14 11:30:56 +0900

    배포 준비 및 오류 수정

--- EmotionDiaryApp/src/pages/New.js
+++ EmotionDiaryApp/src/pages/New.js
@@ -1,10 +1,16 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MyHeader from "./../components/DiaryEditor";
 import DiaryEditor from "./../components/DiaryEditor";
 
 
 const New = () => {
+    
+    useEffect(()=>{
+        const titleElement = document.getElementsByTagName("title")[0];
+        titleElement.innerHTML = `감정 일기장 - 새 일기`;
+    }, []);
+
     return (
         <div>
             <DiaryEditor></DiaryEditor>
