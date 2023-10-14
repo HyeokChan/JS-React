bb68501b920ed00ccaa220a4d92b5e451d4d3320
qpjiq123@gmail.com <qpjiq123@gmail.com>
2023-10-14 10:41:39 +0900

    localstorage 적용

--- EmotionDiaryApp/src/App.js
+++ EmotionDiaryApp/src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from 'react';
+import React, { useEffect, useReducer, useRef } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -33,59 +33,30 @@ const reducer = (state, action) => {
     default:
       return state;
   }
+  localStorage.setItem("diary", JSON.stringify(newState));
   return newState;
 }
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  {
-    id:1,
-    emotion:1,
-    content:"오늘의일기 1번",
-    date:1696329455070,
-  },
-  {
-    id:2,
-    emotion:2,
-    content:"오늘의일기 2번",
-    date:1696329455071,
-  },
-  {
-    id:3,
-    emotion:3,
-    content:"오늘의일기 3번",
-    date:1696329455072,
-  },
-  {
-    id:4,
-    emotion:4,
-    content:"오늘의일기 4번",
-    date:1696329455073,
-  },
-  {
-    id:5,
-    emotion:5,
-    content:"오늘의일기 5번",
-    date:1696329455074,
-  },
-  {
-    id:6,
-    emotion:1,
-    content:"오늘의일기 6번",
-    date:1796329455074,
-  },
-]
 
 function App() {
 
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  useEffect(()=>{
+    const localData = localStorage.getItem("diary");
+    if(localData){
+      const diaryList = JSON.parse(localData).sort((a,b)=>parseInt(b.id) - parseInt(a.id));
+      dataId.current = parseInt(diaryList[0].id) + 1;
+      dispatch({type:"INIT", data:diaryList});
+    }
+  }, []);
 
-  const dataId = useRef(dummyData.length);
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
   // CREATE
   const onCreate = (date, content, emotion) => {
-    dataId.current++;
     dispatch({
       type:"CREATE", 
       data:{
@@ -95,6 +66,7 @@ function App() {
         emotion,
       }
     });
+    dataId.current++;
   }
   // REMOVE
   const onRemove = (targetId) => {
